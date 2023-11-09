8ba57b7e18d564ac564f9504515b2224f5f642d2
gladiseva <lollija.lukasova@gmail.com>
2023-11-09 10:22:59 +0100

    Check for alignment rates: sort, index bam, get flagstat

---
+++ bash_scripts/piepeline/alignment_rates.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#SBATCH --mail-type=fail
+#SBATCH --cpus-per-task=4
+#SBATCH --time=04:00:00
+#SBATCH --mem-per-cpu=8G
+
+module add UHTS/Analysis/samtools/1.10
+
+THREADS=$SLURM_CPUS_PER_TASK 
+
+reads=/data/users/lgladiseva/rna_seq/reads
+reference_genome=/data/courses/rnaseq_course/lncRNAs/Project1/references/GRCh38.genome.fa
+indexed_genome=/data/users/lgladiseva/rna_seq/reference_genome
+mapped_reads=/data/users/lgladiseva/rna_seq/mapped_reads
+
+for file in "$reads"/*_R1_001*.fastq; do
+    # Extract the replicate name from the file name
+    replicate=$(basename "$file" | cut -d'_' -f1,2 | sed 's/_L3//' )
+    mapped_file="$mapped_reads/${replicate}_aligned_reads.bam"
+
+    # Sort and index the BAM file
+    samtools sort -@ $THREADS -o "/data/users/lgladiseva/rna_seq/mapped_reads/${replicate}_aligned_reads.sorted.bam" $mapped_file
+    samtools index "/data/users/lgladiseva/rna_seq/mapped_reads/${replicate}_aligned_reads.sorted.bam"
+
+    # Get alignment statistics
+    samtools flagstat "/data/users/lgladiseva/rna_seq/mapped_reads/${replicate}_aligned_reads.sorted.bam" > "/data/users/lgladiseva/rna_seq/mapped_reads/${replicate}_alignment_stats.txt"
+done
+
+
+
