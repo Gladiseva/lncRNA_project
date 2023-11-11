cd1e533d3a08d8ea191d8c5e5228ba9f0ecf4d73
gladiseva <lollija.lukasova@gmail.com>
2023-11-11 15:54:44 +0100

    Add RF strandedness, save hisat2 output to a file

--- bash_scripts/per_file_test/per_file_reads_mapping.sh
+++ bash_scripts/per_file_test/per_file_reads_mapping.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH --cpus-per-task=8
-#SBATCH --mem-per-cpu=2G
+#SBATCH --mem-per-cpu=8G
 #SBATCH --time=04:00:00
 #SBATCH --partition=pall
 
@@ -11,11 +11,12 @@ THREADS=$SLURM_CPUS_PER_TASK
 
 READS=/data/users/lgladiseva/rna_seq/reads
 INDEXED_GENOME=/data/users/lgladiseva/rna_seq/reference_genome
-MAPPED_READS=/data/users/lgladiseva/rna_seq/mapped_reads 
+MAPPED_READS=/data/users/lgladiseva/rna_seq/mapped_reads
 
-# creates folder if it doesn't exist
-mkdir -p /data/users/lgladiseva/rna_seq/mapped_reads 
+mkdir -p /data/users/lgladiseva/rna_seq/mapped_reads
 
 # align RNA reads to the genome
-# -1 and -2 pair reads (forward and reverse),
-hisat2 -p $THREADS -x $INDEXED_GENOME/GRCh38 -1 $READS/P3_L3_R1_001_fjv6hlbFgCST.fastq -2 $READS/P3_L3_R2_001_xo7RBLLYYqeu.fastq -S $MAPPED_READS/P3_L3.sam
+# -1 and -2 pair reads (forward and reverse)
+# 2>&1 because hisat2 outputs some important info as errors
+# RF -> R1 forward; R2 reverse
+hisat2 -p $THREADS -x $INDEXED_GENOME/GRCh38 -1 $READS/3_2_L3_R1_001_DID218YBevN6.fastq -2 $READS/3_2_L3_R2_001_UPhWv8AgN1X1.fastq -S $MAPPED_READS/3_2_L3_new.sam --rna-strandness RF > $MAPPED_READS/3_2_hisat2_output_new.txt 2>&1
