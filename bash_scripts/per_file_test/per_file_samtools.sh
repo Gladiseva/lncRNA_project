b7d45d6509dd8b18b1a0c2c452472df82d0246bf
gladiseva <lollija.lukasova@gmail.com>
2023-11-11 16:08:07 +0100

    All samtools in one file

--- bash_scripts/per_file_test/per_file_samtools.sh
+++ bash_scripts/per_file_test/per_file_samtools.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
 
-#SBATCH --cpus-per-task=4
-#SBATCH --mem-per-cpu=8G
-#SBATCH --time=00:55:00
-#SBATCH --partition=pall
+#SBATCH --CPUS-PER-TASK=4
+#SBATCH --MEM-PER-CPU=8G
+#SBATCH --TIME=00:55:00
+#SBATCH --PARTITION=PALL
 
 module add UHTS/Analysis/samtools/1.10
 
 THREADS=$SLURM_CPUS_PER_TASK
 
-samtools view -@ $THREADS -bS -o "/data/users/lgladiseva/rna_seq/mapped_reads/P3_aligned_reads.bam" /data/users/lgladiseva/rna_seq/mapped_reads/P3_L3.sam
+# Input and output file paths
+MAPPED_FILE="/data/users/lgladiseva/rna_seq/mapped_reads/3_2_L3_new.sam"
+BAM_FILE="/data/users/lgladiseva/rna_seq/mapped_reads/3_2_L3_new.bam"
+SORTED_BAM="/data/users/lgladiseva/rna_seq/mapped_reads/3_2_aligned_reads_new.sorted.bam"
+ALIGNMENT_STATS="/data/users/lgladiseva/rna_seq/mapped_reads/3_2_new_alignment_stats.txt"
+
+# Convert SAM to BAM
+samtools view -@ $THREADS -b -o $BAM_FILE $MAPPED_FILE
+
+# Sort and index the BAM file
+samtools sort -@ $THREADS -o $SORTED_BAM $BAM_FILE
+samtools index -@ $THREADS $SORTED_BAM
+
+# Get alignment statistics
+samtools flagstat -@ $THREADS $SORTED_BAM > $ALIGNMENT_STATS
