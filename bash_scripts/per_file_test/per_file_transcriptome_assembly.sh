fc63595765fa4f9769f200a4f874166bccb0487d
gladiseva <lollija.lukasova@gmail.com>
2023-11-15 07:50:47 +0100

    Transcriptome assembly

---
+++ bash_scripts/per_file_test/per_file_transcriptome_assembly.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#SBATCH --cpus-per-task=16
+#SBATCH --mem-per-cpu=16G
+#SBATCH --time=04:00:00
+
+module add UHTS/Aligner/stringtie/1.3.3b
+
+
+THREADS=$SLURM_CPUS_PER_TASK
+REPLICATE=$1
+MODE=$2
+FILE=$3
+BAM_FILES="/data/users/lgladiseva/rna_seq/mapped_reads"
+BAM_FILE=${BAM_FILES}/${REPLICATE}_aligned_reads.sorted.bam
+ASSEMBLY_DIR="/data/users/lgladiseva/rna_seq/transcriptome_assembly/${MODE}"
+REFERENCE="/data/courses/rnaseq_course/lncRNAs/Project2/references"
+
+mkdir -p "$ASSEMBLY_DIR"
+
+# reference-guided transcriptome assembly
+# --rf library stranding, -G reference guide annotation
+stringtie -o "$ASSEMBLY_DIR/${REPLICATE}_L3.gtf" -p "$THREADS" --rf -G "$REFERENCE/$FILE" "$BAM_FILE"
---
+++ bash_scripts/piepeline/transcriptome_assembly.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#SBATCH --mail-type=fail
+#SBATCH --cpus-per-task=1
+#SBATCH --time=00:50:00
+#SBATCH --mem-per-cpu=4G
+
+module add UHTS/Aligner/hisat/2.2.1
+module add UHTS/Analysis/samtools/1.10
+
+THREADS=$SLURM_CPUS_PER_TASK 
+
+READS=/data/users/lgladiseva/rna_seq/reads
+
+# Define an array of file types and their corresponding GTF.gz files
+declare -a file_types=("CHR:gencode.v44.annotation.gtf" "ALL:gencode.v44.chr_patch_hapl_scaff.annotation.gtf" "PRI:gencode.v44.primary_assembly.annotation.gtf")
+
+# Iterate through file types
+for type_info in "${file_types[@]}"; do
+    IFS=':' read -r -a type_array <<< "$type_info"
+    type=${type_array[0]}
+    gtf=${type_array[1]}
+
+    # Iterate through files
+    for file in "$READS"/*_R1_001*.fastq; do
+        # Extract the replicate name from the file name
+        replicate=$(basename "$file" | cut -d'_' -f1,2 | sed 's/_L3//' )
+
+        # Submit the job using the replicate name, type, and GTF file
+        sbatch /data/users/lgladiseva/rna_seq/scripts_v2/test_per_file/per_file_transcriptome_assembly.sh "$replicate" "$type" "$gtf"
+    done
+done
