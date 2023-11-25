7a19d1271c584fa6492873e951df485f7a63a1d6
gladiseva <lollija.lukasova@gmail.com>
2023-11-25 13:42:44 +0100

    renaming + change in threads

--- bash_scripts/analysis_pipeline/03_genome_indexing.sh
+++ bash_scripts/analysis_pipeline/03_genome_indexing.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
 #SBATCH --cpus-per-task=8
-#SBATCH --mem-per-cpu=2G
-#SBATCH --time=04:00:00
-#SBATCH --partition=pall
+#SBATCH --mem-per-cpu=16G
+#SBATCH --time=01:00:00
 
 module add UHTS/Aligner/hisat/2.2.1
 
-READS=/data/users/lgladiseva/rna_seq/reads
+THREADS=$SLURM_CPUS_PER_TASK 
 REFERENCE=/data/courses/rnaseq_course/lncRNAs/Project1/references
 INDEXED_GENOME=/data/users/lgladiseva/rna_seq/reference_genome
-THREADS=$SLURM_CPUS_PER_TASK 
 
 # creates the mappgin folder if it doesn't exist yet
 mkdir -p /data/users/lgladiseva/rna_seq/reference_genome
